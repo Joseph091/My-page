b9fab55e1babe0a184fb67c0e3491ed257c1482e
Joseph <anu91233@gmail.com>
2022-06-20 19:03:52 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+console.log(typeof "Joe");
+console.log(typeof 42);
+console.log(typeof true);
+
+let episode = {
+    title: "Dark Beginnings",
+    duration: 45,
+    hasBeenWatched: true,
+};
+
+console.log(episode)
+
+let a = 2;
+let b = 40;
+
+console.log(a+b);
